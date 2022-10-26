384ef15ea9d8600e66cf19e6b208204af0ee164e
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-26 08:51:13 -0300

    dinamico

--- page-post.php
+++ page-post.php
@@ -82,7 +82,10 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
                                 </div>
 
 				<p class="u-font-size-15 u-font-size-18 xl:u-font-size-22 xxl:u-font-size-28 u-font-weight-bold u-font-family-lato u-color-folk-bold-marron mt-3 mb-0">
-					<?php echo $data->date; ?>
+					<?php $date_post = $data->date;
+					   list($data_year, $data_month, $data_day) = explode("-", $date_post);
+					
+					echo  $data_day . '/' . $data_month; ?>
 				</p>
 
 				<!-- <p class="mb-2">
