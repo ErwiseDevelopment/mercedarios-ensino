97fdfa6bcd8a10b95b533e729080cc6ba8cfe82a
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-26 23:49:08 -0300

    dinamico

---
+++ template-parts/content-home-our-differences copy.php
@@ -0,0 +1,129 @@
+<section class="mb-4 py-5">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-12">
+
+                <div class="row">
+
+                    <div class="col-11 col-lg-5 offset-lg-1">
+                        <h3 class="c-title-pattern py-4">
+                            Nossos Diferenciais
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 mt-5 pt-4">
+
+                <div class="row justify-content-center">
+
+                    <a 
+                    class="l-our-differences__col"
+                    href="#">
+
+                        <div class="l-our-differences__item">
+                            <img 
+                            class="img-fluid w-100 u-object-fit-cover"
+                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aulas-de-ingles.png" 
+                            alt="Aulas de inglês">
+                        </div>
+
+                        <div class="l-our-differences__item">
+                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
+                                Aulas de
+                                Inglês
+                            </h4>
+                        </div>
+                    </a>
+
+                    <a 
+                    class="l-our-differences__col"
+                    href="#">
+
+                        <div class="l-our-differences__item">
+                            <img 
+                            class="img-fluid w-100 u-object-fit-cover"
+                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-curso-tecnico.png" 
+                            alt="Cursos Técnicos">
+                        </div>
+
+                        <div class="l-our-differences__item">
+                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
+                                Cursos
+                                Técnicos
+                            </h4>
+                        </div>
+                    </a>
+
+                    <a 
+                    class="l-our-differences__col"
+                    href="#">
+
+                        <div class="l-our-differences__item">
+                            <img 
+                            class="img-fluid w-100 u-object-fit-cover"
+                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-ensino-religioso.png" 
+                            alt="Ensino Religioso">
+                        </div>
+
+                        <div class="l-our-differences__item">
+                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
+                                Ensino Religioso
+                            </h4>
+                        </div>
+                    </a>
+
+                    <a 
+                    class="l-our-differences__col"
+                    href="#">
+
+                        <div class="l-our-differences__item">
+                            <img 
+                            class="img-fluid w-100 u-object-fit-cover"
+                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aula-de-informatica.png" 
+                            alt="Aula de Informática">
+                        </div>
+
+                        <div class="l-our-differences__item">
+                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
+                                Aula de Informática
+                            </h4>
+                        </div>
+                    </a>
+
+                    <a 
+                    class="l-our-differences__col"
+                    href="#">
+
+                        <div class="l-our-differences__item">
+                            <img 
+                            class="img-fluid w-100 u-object-fit-cover"
+                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aula-de-musica.png" 
+                            alt="Aula de Música">
+                        </div>
+
+                        <div class="l-our-differences__item">
+                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
+                                Aula de Música
+                            </h4>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="row justify-content-center">
+
+                    <div class="col-10 col-lg-4 mt-5 pt-4">
+                        <a
+                        class="w-100 rounded-pill d-block u-font-size-16 lg:u-font-size-20 xxl:u-font-size-24 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-bold-marron hover:u-color-folk-white u-bg-folk-light-yellow hover:u-bg-folk-dark-marron py-2"
+                        href="#">
+                            Quero fazer uma visita
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- template-parts/content-home-our-differences.php
+++ template-parts/content-home-our-differences.php
@@ -10,7 +10,7 @@
 
                     <div class="col-11 col-lg-5 offset-lg-1">
                         <h3 class="c-title-pattern py-4">
-                            Nossos Diferenciais
+                            <?php echo get_field('titulo')?>
                         </h3>
                     </div>
                 </div>
@@ -19,98 +19,29 @@
             <div class="col-12 mt-5 pt-4">
 
                 <div class="row justify-content-center">
-
+                <?php 
+                if(have_rows('icones')) :
+                        while (have_rows('icones')) : the_row();
+                
+                ?>
                     <a 
-                    class="l-our-differences__col a:hover"
-                    href="#">
+                    class="l-our-differences__col"
+                    href="<?php echo get_sub_field('link_icon');?>">
 
                         <div class="l-our-differences__item">
                             <img 
                             class="img-fluid w-100 u-object-fit-cover"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aulas-de-ingles.png" 
+                            src="<?php echo get_sub_field('icone_c_fundo')?>" 
                             alt="Aulas de inglês">
                         </div>
 
                         <div class="l-our-differences__item">
                             <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
-                                Aulas de
-                                Inglês
+                                <?php echo get_sub_field('titulo_icon')?>
                             </h4>
                         </div>
                     </a>
-
-                    <a 
-                    class="l-our-differences__col"
-                    href="#">
-
-                        <div class="l-our-differences__item">
-                            <img 
-                            class="img-fluid w-100 u-object-fit-cover"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-curso-tecnico.png" 
-                            alt="Cursos Técnicos">
-                        </div>
-
-                        <div class="l-our-differences__item">
-                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
-                                Cursos
-                                Técnicos
-                            </h4>
-                        </div>
-                    </a>
-
-                    <a 
-                    class="l-our-differences__col"
-                    href="#">
-
-                        <div class="l-our-differences__item">
-                            <img 
-                            class="img-fluid w-100 u-object-fit-cover"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-ensino-religioso.png" 
-                            alt="Ensino Religioso">
-                        </div>
-
-                        <div class="l-our-differences__item">
-                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
-                                Ensino Religioso
-                            </h4>
-                        </div>
-                    </a>
-
-                    <a 
-                    class="l-our-differences__col"
-                    href="#">
-
-                        <div class="l-our-differences__item">
-                            <img 
-                            class="img-fluid w-100 u-object-fit-cover"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aula-de-informatica.png" 
-                            alt="Aula de Informática">
-                        </div>
-
-                        <div class="l-our-differences__item">
-                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
-                                Aula de Informática
-                            </h4>
-                        </div>
-                    </a>
-
-                    <a 
-                    class="l-our-differences__col"
-                    href="#">
-
-                        <div class="l-our-differences__item">
-                            <img 
-                            class="img-fluid w-100 u-object-fit-cover"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-aula-de-musica.png" 
-                            alt="Aula de Música">
-                        </div>
-
-                        <div class="l-our-differences__item">
-                            <h4 class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron">
-                                Aula de Música
-                            </h4>
-                        </div>
-                    </a>
+                    <?php endwhile; endif;?>
                 </div>
 
                 <div class="row justify-content-center">
@@ -118,7 +49,7 @@
                     <div class="col-10 col-lg-4 mt-5 pt-4">
                         <a
                         class="w-100 rounded-pill d-block u-font-size-16 lg:u-font-size-20 xxl:u-font-size-24 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-bold-marron hover:u-color-folk-white u-bg-folk-light-yellow hover:u-bg-folk-dark-marron py-2"
-                        href="#">
+                        href="<?php get_field('link_visita')?>">
                             Quero fazer uma visita
                         </a>
                     </div>
