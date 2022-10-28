5e2f1f38cd164aba8b582d5eeda68d6fb2542b0a
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-28 16:08:53 -0300

    correcao

--- page-fotos.php
+++ page-fotos.php
@@ -20,22 +20,32 @@ get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
 
-<section 
-class="l-template-content__banner d-flex justify-content-center align-items-center u-bg-cover u-bg-no-repeat"
-style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/template-content-banner.png)">
+<!-- banner -->
+<section
+class="u-border-color-dark-marron u-bg-cover u-bg-no-repeat py-5" 
+style="border-top:20px solid;background-image:url(<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/banner-title.png)">
 
     <div class="container">
 
         <div class="row">
 
-            <div class="col-12 px-0">
-                <h1 class="l-template-content__banner__title position-relative u-font-weight-bold u-font-family-cinzel-decorative text-center u-color-folk-white pb-4">
-                    ALBUNS
-                </h1>
+            <div class="col-12">
+
+                <div class="d-flex justify-content-center align-items-center">
+                    <img 
+                    class="img-fluid"
+                    src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/icon-plus-title.png" 
+                    alt="Ícone mais">
+
+                    <h1 class="d-inline-block u-font-size-32 xl:u-font-size-48 xxl:u-font-size-54 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-white mb-0 ml-3">
+                        Albuns
+                    </h1>
+                </div>
             </div>
         </div>
     </div>
 </section>
+
 <!-- end banner -->
 <section class="mb-5">
 
