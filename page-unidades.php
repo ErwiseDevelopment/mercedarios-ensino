906ff60917b8ce42df6641ee63597d89cca81121
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-27 23:57:15 -0300

    correcao

--- page-unidades.php
+++ page-unidades.php
@@ -96,7 +96,7 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
         </div>
     </div>
 </section>
-<?php }; else: echo get_template_part( 'sem-acesso.php' ); endif;?>
+<?php }; else: echo get_template_part( 'sem-acesso' ); endif;?>
 <!-- end units -->
 
 <!-- banner material -->
