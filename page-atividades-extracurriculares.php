6090e4a6deaa133a76c8356ecf556694980f1e7c
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-31 00:23:24 -0300

    ajuste extracurricular,

--- page-atividades-extracurriculares.php
+++ page-atividades-extracurriculares.php
@@ -102,7 +102,14 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
                         </div>
 
                         <div class="col-12 mt-4">
-                            
+                        <?php
+                        $images = get_field( 'galeria_curso_extra' );
+                        $count = -1;
+
+                        if( $images ) :
+                            foreach( $images as $image ) :
+                                $count++;
+                    ?>
                             <!-- swiper -->
                             <div class="swiper-container js-swiper-activities-all js-swiper-activities-<?php echo $i; ?>">
 
@@ -110,7 +117,7 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
                                         <div class="swiper-slide">
                                             <img
                                             class="img-fluid w-100 u-object-fit-cover"
-                                            src="<?php echo get_field('galeria_curso_extra')?>"
+                                            src="<?php echo $image[ 'url' ]; ?>"
                                             alt="Atividade"> 
                                         </div> 
                                 </div>
@@ -120,6 +127,10 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
                             <div class="swiper-button-prev swiper-button-prev-activities js-swiper-button-prev-activities-<?php echo $i; ?>"></div>
                             <div class="swiper-button-next swiper-button-next-activities js-swiper-button-next-activities-<?php echo $i; ?>"></div>
                             <!-- end swiper -->
+                            <?php
+                                endforeach;
+                            endif;
+                        ?>
                         </div>
                     </div>
                 </div>
@@ -130,6 +141,52 @@ style="border-top:20px solid;background-image:url(<?php echo get_template_direct
     </div>
 </section>
 <!-- end contents -->
+
+<!-- modal photos -->
+<div class="l-modal-photos d-flex justify-content-center align-items-center js-modal-photos">
+    
+    <div class="l-modal-photos__overlay js-modal-photos-overlay"></div>
+    <span class="l-modal-photos__close js-modal-photos-close">x</span>
+
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-md-10 col-lg-8">
+
+                <!-- swiper -->
+                <div class="swiper-container js-swiper-modal-photos">
+
+                    <div class="swiper-wrapper">
+                        
+                        <!-- slide -->
+                        <?php
+                            if( $images ) :
+                                foreach( $images as $image ) :
+                        ?>
+                                    <div class="swiper-slide">
+                                        <img
+                                        class="l-modal-photos__image img-fluid w-100"
+                                        src="<?php echo $image[ 'url' ]; ?>"
+                                        alt="<?php the_title() ?>">
+                                    </div>
+                        <?php
+                                endforeach;
+                            endif;
+                        ?>
+                        <!-- end slide -->
+                    </div>
+                </div>
+
+                <!-- arrows -->
+                <div class="swiper-button-prev swiper-button-prev-modal-photos u-color-folk-white js-swiper-button-prev-modal-photos"></div>
+                <div class="swiper-button-next swiper-button-next-modal-photos js-swiper-button-next-modal-photos"></div>
+                <!-- end swiper -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end modal photos -->
 
 <!-- other posts -->
 <section class="l-teaching has-not-center mb-5 py-5">
