240b14ad18d0a1da6331f5bf73a0df1355a7605d
ErwiseDevelopment <erwise.dev@gmail.com>
2022-11-17 00:57:07 -0300

    #KPV-300683 - Alterações Mercês

--- footer.php
+++ footer.php
@@ -178,7 +178,7 @@
                             <?php } endif;?>   
                         </ul>
                 
-                <div class="col-lg-4 my-4 my-lg-0">
+               
                          <div class="d-flex justify-content-between justify-content-lg-center mb-0 px-5 px-lg-0">
                             <p class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-white"><?php echo get_field('escola2', 'option')?></p>
                         </div>                    
@@ -229,7 +229,8 @@
                             </li>
                             <?php } endif;?>   
                         </ul>
-                </div></div>
+                   
+                </div>
             </div>
 
             <div class="row justify-content-center">
