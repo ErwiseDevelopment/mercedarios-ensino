4e041ee5bfcc06a88363ae73ba12c4537983fddd
ErwiseDevelopment <erwise.dev@gmail.com>
2022-11-17 00:01:34 -0300

    #KPV-300683 - Alterações Mercês

--- template-parts/content-top-info.php
+++ template-parts/content-top-info.php
@@ -37,7 +37,9 @@
                             </div>
                         </div>
                         <?php } endif;?>
-                        <div class="pb-5"><p>piro231</p></div>          
+                        <div class="d-flex justify-content-between justify-content-lg-center mb-0 px-5 px-lg-0">
+                            <p class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron"><?php echo get_field('escola1', 'option')?></p>
+                        </div>          
                         <div>
                             <ul class="d-flex justify-content-between justify-content-lg-center mb-0 px-5 px-lg-0">
                           
@@ -84,47 +86,49 @@
                                 <?php } endif; ?> 
                             </ul>
                         </div>
-                        <div class="pb-5"><p>piro231</p></div>    
+                        <div class="d-flex justify-content-between justify-content-lg-center mb-0 px-5 px-lg-0">
+                            <p class="u-font-size-22 xxl:u-font-size-24 u-font-weight-extrabold u-font-family-nunito text-center u-color-folk-dark-marron"><?php echo get_field('escola2', 'option')?></p>
+                        </div>   
                         <div>
 
                             <ul class="d-flex justify-content-between justify-content-lg-center mb-0 px-5 px-lg-0">
                                
-                                <?php if (!empty(get_field('facebook_redes', 'option'))):{?>
+                                <?php if (!empty(get_field('facebook_redes_2', 'option'))):{?>
                                     <li class="u-list-style-none mr-2">
                                         <a
                                         class="px:u-w-32 px:u-h-32 u-icon__brands u-icon__facebook rounded-pill d-flex justify-content-center align-items-center u-font-size-0 before::u-font-size-18 u-font-weight-regular text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden" rel="noreferrer noopener"                               
-                                        href="<?php echo get_field('facebook_redes', 'option')?>" <?php if (get_field('nova_guia_face', 'option') == '1') :?>  target="_blank" <?php endif; ?>
+                                        href="<?php echo get_field('facebook_redes_2', 'option')?>" <?php if (get_field('nova_guia_face_2', 'option') == '1') :?>  target="_blank" <?php endif; ?>
                                         target="_blank"
                                         rel="noreferrer noopener">
                                             Link do Facebook
                                         </a>
                                     </li>
                                 <?php } endif;?>
-                                <?php if (!empty(get_field('instagram_redes', 'option'))):{?>
+                                <?php if (!empty(get_field('instagram_redes_2', 'option'))):{?>
                                     <li class="u-list-style-none mr-2">
                                         <a
                                         class="px:u-w-32 px:u-h-32 u-icon__brands u-icon__instagram  rounded-pill d-flex justify-content-center align-items-center u-font-size-0 before::u-font-size-18 u-font-weight-regular text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden" rel="noreferrer noopener"                               
-                                        href="<?php echo get_field('instagram_redes', 'option')?>" <?php if (get_field('nova_guia_insta', 'option') == '1') :?>  target="_blank" <?php endif; ?>
+                                        href="<?php echo get_field('instagram_redes_2', 'option')?>" <?php if (get_field('nova_guia_insta_2', 'option') == '1') :?>  target="_blank" <?php endif; ?>
                                         target="_blank"
                                         rel="noreferrer noopener">
                                             Link do Instagram
                                         </a>
                                     </li>
                                 <?php } endif;?>
-                                <?php if (!empty(get_field('youtube_redes', 'option'))):{?>             
+                                <?php if (!empty(get_field('youtube_redes_2', 'option'))):{?>             
                                     <li class="u-list-style-none mr-2">
                                         <a
                                         class="px:u-w-32 px:u-h-32 u-icon__brands u-icon__youtube rounded-pill d-flex justify-content-center align-items-center u-font-size-0 before::u-font-size-18 u-font-weight-regular text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden" rel="noreferrer noopener"                               
-                                        href="<?php echo get_field('youtube_redes', 'option')?>" <?php if (get_field('nova_guia_yt', 'option') == '1') :?>  target="_blank" <?php endif; ?> >
+                                        href="<?php echo get_field('youtube_redes_2', 'option')?>" <?php if (get_field('nova_guia_yt_2', 'option') == '1') :?>  target="_blank" <?php endif; ?> >
                                             Link do Youtube
                                         </a>
                                     </li>
                                 <?php } endif;?>
-                                <?php if (!empty(get_field('whatsapp_redes', 'option'))):{?>                    
+                                <?php if (!empty(get_field('whatsapp_redes_2', 'option'))):{?>                    
                                     <li class="u-list-style-none mr-2">
                                         <a
                                         class="px:u-w-32 px:u-h-32 u-icon__brands u-icon__whatsapp rounded-pill d-flex justify-content-center align-items-center u-font-size-0 before::u-font-size-18 u-font-weight-regular text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden" rel="noreferrer noopener"                               
-                                        href="<?php echo get_field('whatsapp_redes', 'option')?>" <?php if (get_field('nova_guia_wpp', 'option') == '1') :?>  target="_blank" <?php endif; ?> >
+                                        href="<?php echo get_field('whatsapp_redes_2', 'option')?>" <?php if (get_field('nova_guia_wpp_2', 'option') == '1') :?>  target="_blank" <?php endif; ?> >
 
                                        Link do Whatsapp
                                         </a>
